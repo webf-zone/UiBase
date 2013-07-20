b33a45cc55bbd245db3c0157eed82ccf8995c9da
Debjit Biswas <debjitbis08@gmail.com>
2013-07-20 18:08:54 +0530

    Add another label to the test view

--- uibase/views/test.js
+++ uibase/views/test.js
@@ -17,14 +17,17 @@
         });
 
         v.lbl = new ub.Views.Label({ text: "No Value" });
+        v.anotherLbl = new ub.Views.Label({ text: "No Value" });
 
         ub.Component.connect(v.lbl, "text", v.select.get("value"));
+        ub.Component.connect(v.anotherLbl, "text", v.select.get("value").map(function(v) { return parseInt(v,10) + 1; }));
     });
 
     Test.prototype.render = function() {
         return $("<div>")
             .append(this.select.render())
-            .append(this.lbl.render());
+            .append(this.lbl.render())
+            .append(this.anotherLbl.render());
     };
 
     ub.Views.Test = Test;
