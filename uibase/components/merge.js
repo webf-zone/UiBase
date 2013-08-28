fd41f49628a79b9750023f0e05ba6ccce6fb8c1b
Debjit Biswas <debjitbis08@gmail.com>
2013-08-29 01:46:44 +0530

    Add merge and sampleOn components.
    Merge component merger two observables into one.
    
    SampleOn component get the value input whenevet the sampleOn input
    changes.

---
+++ uibase/components/merge.js
@@ -0,0 +1,46 @@
+;(function(ub) {
+    "use strict";
+
+    ub.Components = ub.Components || {};
+
+    var Merge = ub.Utils.Class({
+
+        extends: ub.Component,
+
+        construct: function() {
+            var self = this;
+
+            self._super();
+
+            self._outPorts = {
+                output: new ub.Observable(function(observer) {
+                    self._observer = observer;
+                })
+            };
+
+            self._inPorts = {
+                stream1: new ub.Observer(function() {
+                    self._update.apply(self, Array.prototype.slice.call(arguments, 0));
+                },
+                function(errors) {
+                    //TODO: Define this function
+                    self._error(errors);
+                },
+                function() {
+                    //TODO: Define this function
+                    self._completed();
+                }),
+                stream2: new ub.Observer(function(val) {
+                    self._update.apply(self, Array.prototype.slice.call(arguments, 0));
+                })
+            };
+        },
+
+        _update: function() {
+            this._observer.onNext.apply(this._observer, Array.prototype.slice.call(arguments, 0));
+        }
+    });
+
+    ub.Components.Merge  = Merge;
+
+})(window.uibase);
